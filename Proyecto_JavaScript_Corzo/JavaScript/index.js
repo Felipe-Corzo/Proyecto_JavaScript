48f3ae6fe160dba2bdeb8384fa867806f15d89cf
Felipe-Corzo <felipecorzo47@gmail.com>
2025-12-17 11:07:14 -0500

    fix: Se mejoró el diseño de los product-cards

--- Proyecto_JavaScript_Corzo/JavaScript/index.js
+++ Proyecto_JavaScript_Corzo/JavaScript/index.js
@@ -15,12 +15,21 @@ async function getAllProducts() {
             productElement.classList.add('product');
             productElement.innerHTML = `
                  <div class='card'>
-                    <a href='details.html?id=${product.id}'>
-                        <img class='product-image' src='${product.image}' alt='${product.title}'>
-                        <h1 class='{product-title' >${product.title}</h1>
-                    </a>
-                    <p class='product-price'>$${product.price}</p>
-                    <button class='product-add' type='button' onclick='addToCar(${product.id})'>Añadir al carrito</button>
+                    <p class='product-price'>US$ ${product.price}</p>                    
+                    <img class='product-image' src='${product.image}' alt='${product.title}'>
+                    <p class='product-category' >${product.category}</p>
+                    <h1 class='product-title' >${product.title}</h1>                    
+                    
+                    <div class='card-footer'>
+                        <button class='details-button' type='button' onclick='showDetails(${product.id})'>Ver Detalles</button>
+                        <button class='product-add' type='button' onclick='addToCar(${product.id})'>
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <circle cx="9" cy="21" r="1"></circle>
+                            <circle cx="20" cy="21" r="1"></circle>
+                            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                        </svg>
+                        </button>
+                    </div>
                  </div>
                 `;
             productosContainer.appendChild(productElement);
@@ -40,6 +49,11 @@ function addToCar(productId) {
 
     addProductToCart(product, 1);
     showToCartNotification(product.title);
+}
+
+function showDetails(productId) {
+    window.location.href = `details.html?id=${productId}`;
+
 }
 
 updateCarstCount();
